bd6a1d7418d696ddee6678e9c000cc25f2554e48
Eddy Keenan <keee180@gmail.com>
2023-04-22 17:54:01 -0400

    Cleaned up code

--- mass-release-bootstrap.js
+++ mass-release-bootstrap.js
@@ -1,20 +1,20 @@
 var mass_release_script = document.createElement("script");
 // chromium-based browsers:
 try {
-    mass_release_script.src = chrome.runtime.getURL("mass-release-script.js");
+   mass_release_script.src = chrome.runtime.getURL("mass-release-script.js");
 } catch {
-    // firefox and safari browsers:
-    try {
-        mass_release_script.src = browser.runtime.getURL("mass-release-script.js");
-    }
-    catch (e) {
-        console.log(e);
-        alert("Sorry, your browser's method of accessing an extension's \
+   // firefox and safari browsers:
+   try {
+      mass_release_script.src = browser.runtime.getURL("mass-release-script.js");
+   }
+   catch (e) {
+      console.log(e);
+      alert("Sorry, your browser's method of accessing an extension's \
          web-accessible resources is not supported.");
-    }
+   }
 }
 
-mass_release_script.onload = function() {
-    this.remove();
+mass_release_script.onload = function () {
+   this.remove();
 };
 (document.head || document.documentElement).appendChild(mass_release_script);
