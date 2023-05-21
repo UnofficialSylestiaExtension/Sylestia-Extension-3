ade2c028a6e201a909a72e7af7b6c7f3f461d42b
Eddy Keenan <keee180@gmail.com>
2023-05-21 17:41:52 -0400

    Added hotkeys to fest zones

---
+++ z1-explore-hotkey-loader.js
@@ -0,0 +1,24 @@
+'use strict'
+function loadScript(name) {
+   var loaded_script = document.createElement("script");
+   // chromium-based browsers:
+   try {
+      loaded_script.src = chrome.runtime.getURL(name);
+   } catch {
+      // firefox and safari browsers:
+      try {
+         loaded_script.src = browser.runtime.getURL(name);
+      }
+      catch (e) {
+         console.log(e);
+         alert("Sorry, your browser's method of accessing an extension's \
+            web-accessible resources is not supported.");
+      }
+   }
+
+   loaded_script.onload = function () {
+      this.remove();
+   };
+   (document.head || document.documentElement).appendChild(loaded_script);
+}
+loadScript("z1-explore-hotkey-script.js");
